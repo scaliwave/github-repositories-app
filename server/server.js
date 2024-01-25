55f18ba3c5c21bf2d44d77cac2b0188753192934
Thms <thomasog10@gmail.com>
2024-01-25 16:09:51 -0500

    update settings v.1.5

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 // ruta para servir al cliente
